2c3e2dc093d9d37bf2a5b65bc0252b4738c08603
iammrmoo@gmail.com <iammrmoo@gmail.com>
2023-05-04 22:53:14 -0400

    finally it works

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,27 +1,28 @@
-// Assignment Code
+// Assignment Code Do Not Change
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+// Writes password to the #password input
 function writePassword() {
   var passwordText = document.querySelector("#password");
   var passwordLength = parseInt(prompt("Choose a password length between 8 and 128 characters"));
-
-  // Check if the user entered a valid number for password length
+  // Checks if the user entered a valid number for password length ^^^ return if invalid
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid number between 8 and 128.");
     return;
-  }
+  } 
+
 
   var includeLowercase = confirm("Do you want to include lowercase letters?");
   var includeUppercase = confirm("Do you want to include uppercase letters?");
   var includeNumbers = confirm("Do you want to include numbers?");
   var includeSpecial = confirm("Do you want to include special characters?");
-
-  // Check if the user selected at least one character type
+  // Check if the user selected at least one character type ^^^ return if invalid
   if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
     alert("Please select at least one character type.");
     return;
   }
+
 
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -55,5 +56,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Add event listener to 'generate password' button
 generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,11 @@
+This is my JavaScript for generating a random password with certain criteria.
+I used HTML and CSS provided to me from class.
+When you select certain character critera the code should provide a random generated password that fits selected critera.
+
+References:
+https://youtu.be/iKo9pDKKHnc
+https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+https://stackoverflow.com/questions/1497481/javascript-password-generator
+
+Live at:
+
