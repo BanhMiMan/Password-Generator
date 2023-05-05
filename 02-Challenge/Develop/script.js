1f7e323a3ffefdf346df67cd5d33448b9ec7928d
iammrmoo@gmail.com <iammrmoo@gmail.com>
2023-05-04 23:03:33 -0400

    Okay now its good

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 // Writes password to the #password input
 function writePassword() {
   var passwordText = document.querySelector("#password");
-  var passwordLength = parseInt(prompt("Choose a password length between 8 and 128 characters"));
+  var passwordLength = parseInt(prompt("Choose a password length between 8-128 characters"));
   // Checks if the user entered a valid number for password length ^^^ return if invalid
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid number between 8 and 128.");
@@ -18,6 +18,8 @@ function writePassword() {
   var includeNumbers = confirm("Do you want to include numbers?");
   var includeSpecial = confirm("Do you want to include special characters?");
   // Check if the user selected at least one character type ^^^ return if invalid
+  // In JavaScript exclamation points are the logical negation ooperator.
+  // Meaning they return the opposite of a boolean value (!true = false)
   if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
     alert("Please select at least one character type.");
     return;
